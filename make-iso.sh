408032a696bd2c0620dbcf3aa10591215991eac2
Maxwell Leisner <coffeeframe@linux.com>
2018-03-20 08:22:58 -0400

    update for fedora 27 and variablize fedora version and architecture

--- make-iso.sh
+++ make-iso.sh
@@ -10,7 +10,8 @@ set -eu
 ####################################################################
 green=`tput setaf 2`
 reset=`tput sgr0`
-#to use mock comment out lines 25, 26 and 30 and uncomment all lines beginning with mock
+fedora_version=27
+arch="x86_64"
 echo "${green}Welcome to the TigerOS build script${reset}"
 # Check that the current user is root
 if [ $EUID != 0 ]
@@ -18,10 +19,10 @@ then
     echo "Please run this script as root (sudo $@$0)."
     exit
 fi
-dnf install -y lorax-lmc-novirt git vim-minimal pykickstart
-dnf install -y https://tigeros.ritlug.com/packages/x86_64/anaconda-installclass-tigeros-26-1.fc26.x86_64.rpm
+#dnf install -y lorax-lmc-novirt git vim-minimal pykickstart
+#dnf install -y https://tigeros.ritlug.com/packages/$arch/anaconda-installclass-tigeros-$fedora_version-1.fc$fedora_version.$arch.rpm
 echo "${green}Beginning build process${reset}"
 setenforce 0
-livemedia-creator --ks tigeros.ks --no-virt --resultdir /var/lmc --project TigerOS-Live --make-iso --volid TigerOS --iso-only --iso-name TigerOS.iso --releasever 26 --title TigerOS-live --macboot
+livemedia-creator --ks tigeros.ks --no-virt --resultdir /var/lmc --project TigerOS-Live --make-iso --volid TigerOS --iso-only --iso-name TigerOS.iso --releasever $fedora_version --title TigerOS-live --macboot
 echo "${green}ISO saved to $(pwd)/TigerOS.iso${reset}"
 setenforce 1
